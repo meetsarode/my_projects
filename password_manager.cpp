7105fa2db5d13cfd7abdcdadc393a5c6ec1887ab
Meet Sarode <sarodemeet22@gmail.com>
2026-08-08 12:38:47 +0530

    adding function..

--- password_manager.cpp
+++ password_manager.cpp
@@ -20,23 +20,58 @@ class passmanager{
         vector<user> listofusers;
 
     public:
+         void xor_Encrypt_Decrypt(string &data ,const char &key);
         void savetoFile();
         void loadFromFile();
         void mainMenu();
-        void adduser();
-        void deleteuser();
-        void updateuser();
-        void displayuser();
-        void searchuser();
-        void xor_Encrypt_Decrypt(string &data ,const string &key);
+        void addUser();
+        void deleteUser();
+        void updateUser();
+        void displayUser();
+        void searchUser();
         void login_main_menu();
-        void regiuser();
+        void regiUser();
         void login();
         void logout();
     
 
 };
 
+void passmanager::savetoFile(){
+    ofstream file(LoginUserName + ".txt");
+    if (file.is_open())
+    {
+        file<<LoginUserName<<"\n";
+        file<<Loginpassword<<"\n";
+        for (const auto& user :listofusers)
+        {
+            file << user.userName <<"\n";
+            file << user.password <<"\n";
+            file << user.accountName <<"\n";
+        }
+        file.close();
+    }else
+    {
+        cout<< "Unable to open file for writing."<<endl;
+    }
+
+}
+
+void passmanager::loadFromFile(){
+    ifstream file(LoginUserName + ".txt");
+    if (file.is_open()){
+        getline(file,LoginUserName);
+        getline(file,Loginpassword);
+        user user;
+        while (getline(file ,user.userName) && getline(file, user.password) && getline(file,user.accountName))
+        {
+            listofusers.push_back(user);
+        }
+        file.close();
+    }else{
+        cout << "Unable to open file for reading." <<endl;
+    }
+}
 
 int main(){
     cout<<"hello world";
